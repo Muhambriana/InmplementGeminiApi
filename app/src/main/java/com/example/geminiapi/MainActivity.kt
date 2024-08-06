83ae27c89f9fe424ea4186b53f211f821f617027
Muham <muhambriana.31@gmail.com>
2024-08-06 14:12:19 +0700

    generate text from text-and-image input (multimodal)

--- app/src/main/java/com/example/geminiapi/MainActivity.kt
+++ app/src/main/java/com/example/geminiapi/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.geminiapi
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.geminiapi.databinding.ActivityMainBinding
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -35,14 +38,32 @@ class MainActivity : AppCompatActivity() {
         )
 
         CoroutineScope(Dispatchers.Main).launch {
-            generateTextFromTextInput(generativeModel)
+            binding.tvMain.text = generateTextFromTextAndImageInput(generativeModel)
         }
     }
 
     // Generate text from text-only input
-    private suspend fun generateTextFromTextInput(generativeModel: GenerativeModel) {
-        val prompt = "Who is the ceo of google in 2020."
+    private suspend fun generateTextFromTextInput(generativeModel: GenerativeModel): String? {
+        val prompt = "Apakah kamu bisa berbahasa indonesia?"
         val response = generativeModel.generateContent(prompt)
-        binding.tvMain.text = response.text
+        return response.text
+    }
+
+    // Generate text from text-and-image input (multimodal)
+    private suspend fun generateTextFromTextAndImageInput(generativeModel: GenerativeModel): String? {
+        val text1 = "What's different between these pictures?"
+        val text2 = "What the content of the image?"
+        val image1: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.linux)
+        val image2: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.linux)
+
+        val inputContent = content {
+            image(image1)
+            image(image2)
+            text(text1)
+            text(text2)
+        }
+
+        val response = generativeModel.generateContent(inputContent)
+        return response.text
     }
 }
