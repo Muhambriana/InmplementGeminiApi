4833659265900ed7c296e082e4300658860e1699
Muham <muhambriana.31@gmail.com>
2024-10-18 09:45:14 +0700

    add new libs and migrate to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+import java.util.Properties
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+// Load the API key from local.properties
+val aiStudioApiKey: String by lazy {
+    rootProject.file("local.properties").let { propertiesFile ->
+        Properties().apply {
+            if (propertiesFile.exists()) load(propertiesFile.inputStream())
+        }.getProperty("aiStudioApiKey", "")
+    }
+}
+
+android {
+    namespace = "com.example.geminiapi"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.geminiapi"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 4
+        versionName = "1.1.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // Add the API key as a BuildConfig field
+        buildConfigField("String", "AI_STUDIO_API_KEY", aiStudioApiKey)
+    }
+
+    buildTypes {
+        release {
+            isDebuggable = false
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Gen AI
+    implementation(libs.generativeAi)
+
+    implementation(libs.ssp.android)
+    implementation(libs.sdp.android)
+    implementation(libs.lifecycle.viewmodel.ktx)
+}
