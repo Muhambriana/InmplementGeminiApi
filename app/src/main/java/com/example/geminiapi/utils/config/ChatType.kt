cd793cc323fe0606982a9135bffbb7e7713a552d
Muham <muhambriana.31@gmail.com>
2024-10-20 08:03:51 +0700

    update ChatType

--- app/src/main/java/com/example/geminiapi/utils/config/ChatType.kt
+++ app/src/main/java/com/example/geminiapi/utils/config/ChatType.kt
@@ -1,6 +1,10 @@
 package com.example.geminiapi.utils.config
 
-enum class ChatType {
-    USER,
-    MODEL
+enum class ChatType(val type: String, val alias: String) {
+    USER("User", "You"),
+    MODEL("Model", "Gemini");
+
+    override fun toString(): String {
+        return type
+    }
 }
