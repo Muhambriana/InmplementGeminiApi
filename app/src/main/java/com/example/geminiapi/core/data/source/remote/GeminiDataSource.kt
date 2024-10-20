e314a0fbc8f14df49a7cced235e22251d0e4a487
Muham <muhambriana.31@gmail.com>
2024-10-20 09:22:54 +0700

    add safety settings

--- app/src/main/java/com/example/geminiapi/core/data/source/remote/GeminiDataSource.kt
+++ app/src/main/java/com/example/geminiapi/core/data/source/remote/GeminiDataSource.kt
@@ -4,30 +4,43 @@ import com.example.geminiapi.BuildConfig
 import com.example.geminiapi.utils.config.Prompt
 import com.google.ai.client.generativeai.Chat
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.BlockThreshold
 import com.google.ai.client.generativeai.type.Content
 import com.google.ai.client.generativeai.type.GenerateContentResponse
+import com.google.ai.client.generativeai.type.HarmCategory
+import com.google.ai.client.generativeai.type.SafetySetting
 import kotlinx.coroutines.flow.Flow
 
 
 class GeminiDataSource {
+    private val harassmentSafety = SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.MEDIUM_AND_ABOVE)
+    private val hateSpeechSafety = SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.LOW_AND_ABOVE)
     private val generativeModel: GenerativeModel = GenerativeModel(
         modelName = "gemini-1.5-flash",
-        apiKey = BuildConfig.AI_STUDIO_API_KEY
+        apiKey = BuildConfig.AI_STUDIO_API_KEY,
+        safetySettings = listOf(harassmentSafety, hateSpeechSafety)
     )
 
     suspend fun getStartChat(history: List<Content>): Chat =
         generativeModel.startChat(history)
 
-    suspend fun getStreamMessage(chat: Chat?, prompt: Prompt): Flow<GenerateContentResponse>? =
-        when(prompt) {
-            is Prompt.TextPrompt -> {
-                chat?.sendMessageStream(prompt.text)
-            }
-            is Prompt.BitmapPrompt -> {
-                chat?.sendMessageStream(prompt.bitmap)
-            }
-            is Prompt.ContentPrompt -> {
-                chat?.sendMessageStream(prompt.content)
-            }
+    suspend fun getStreamMessage(chat: Chat?, prompt: Prompt): Result<Flow<GenerateContentResponse>?> =
+        try {
+            Result.success(
+                when(prompt) {
+                    is Prompt.TextPrompt -> {
+                        chat?.sendMessageStream(prompt.text)
+                    }
+                    is Prompt.BitmapPrompt -> {
+                        chat?.sendMessageStream(prompt.bitmap)
+                    }
+                    is Prompt.ContentPrompt -> {
+                        chat?.sendMessageStream(prompt.content)
+                    }
+                }
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.failure(e)
         }
 }
